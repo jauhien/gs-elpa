7a8c8dab5e4e079943d0ac2ecb8e9557ae725a49
Jauhien Piatlicki <jauhien@gentoo.org>
2014-12-04 22:13:02 +0100

    0.1.3 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name          = 'gs-elpa',
-      version       = '0.1.2',
+      version       = '0.1.3',
       description   = 'g-sorcery backend for elisp packages',
       author        = 'Jauhien Piatlicki',
       author_email  = 'jauhien@gentoo.org',
